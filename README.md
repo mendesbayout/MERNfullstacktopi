b96a5b1ad63ae63b111d2fe49eb7d3d5b5fc134c
mendesbayout <62385787+mendesbayout@users.noreply.github.com>
2020-10-05 18:53:58 -0300

    v1.4 MERN fullstack

---
+++ README.md
@@ -0,0 +1,25 @@
+# App for topi selection process to fetch and list the most stared repo for a desired language.
+@Javascript was choosen (Não entendi muito bem, se era pra usar o link do rep Java)
+
+React.js - Front-end.
+Node.js - Back-end.
+Express.jr for routes
+MongoDB implemented (missing SQLite application for local storage)
+Missing dropdown menu for language selection (however google localization working)
+
+## Instalação
+
+NPM installed execute:
+
+    npm run client-install
+    
+
+Development:
+
+	npm run dev (Load both front + end)
+
+Production:
+
+    npm run heroku-postbuild
+
+
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.use(express.json());
+
+//Use routers
+
+app.use('/users', require('./routes/api/users'));
+app.use('/register', require('./routes/api/auth'));
+
+
+
+//DB config  
+const db = require('./config/keys').mongoURI;
+
+//Connect mongoES
+mongoose
+  .connect(db, { useCreateIndex: true, useNewUrlParser: true,  useUnifiedTopology: true })
+  .then(() => console.log('DB connected'))
+  .catch(err => console.log(err));
+
+
+
+// Serve static if in products assts
+if(process.env.NODE_ENV === 'production'){
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+  });
+}
+
+
+
+
+
+const port = process.env.PORT || 5000;
+
+
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
